7c692ad0a8e6ede6d70e979363100c830297e7dd
Sapphyruss <lugi.franklin@outlook.fr>
2024-03-28 07:15:32 -0700

    .

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,39 +1,88 @@
 #!/usr/bin/python3
-""" log parsing task """
-import sys
+'''parsing HTTP request logs.
+'''
 import re
 
 
-def do_print(size, codes):
-    """ print the parsed log """
-    print("File size: {}".format(size))
-    for code, count in codes.items():
-        if count > 0:
-            print("{}: {}".format(code, count))
+def extract_input(input_line):
+    '''Extracts sections of a line of an HTTP request log.
+    '''
+    fp = (
+        r'\s*(?P<ip>\S+)\s*',
+        r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
+        r'\s*"(?P<request>[^"]*)"\s*',
+        r'\s*(?P<status_code>\S+)',
+        r'\s*(?P<file_size>\d+)'
+    )
+    info = {
+        'status_code': 0,
+        'file_size': 0,
+    }
+    log_fmt = '{}\\-{}{}{}{}\\s*'.format(fp[0], fp[1], fp[2], fp[3], fp[4])
+    resp_match = re.fullmatch(log_fmt, input_line)
+    if resp_match is not None:
+        status_code = resp_match.group('status_code')
+        file_size = int(resp_match.group('file_size'))
+        info['status_code'] = status_code
+        info['file_size'] = file_size
+    return info
 
 
-# initialize the states
-total_size = 0
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0,
-                405: 0, 500: 0}
-regex = re.compile(r'''(\d{1, 3}\.\d{1, 3}.\d{1, 3}.\d{1, 3}) - \[(.*?)\]
-        "GET /projects/260 HTTP/1.1" (/d+) (\d+)''', re.X)
+def print_statistics(total_file_size, status_codes_stats):
+    '''Prints the accumulated statistics of an HTTP request log.
+    '''
+    print('File size: {:d}'.format(total_file_size), flush=True)
+    for status_code in sorted(status_codes_stats.keys()):
+        num = status_codes_stats.get(status_code, 0)
+        if num > 0:
+            print('{:s}: {:d}'.format(status_code, num), flush=True)
 
-line_count = 0  # keep track of the line count
 
-try:
-    for line in sys.stdin:
-        match = re.match(regex, line)
-        if True:  # placeholder for the regex
-            total_size += int(line.split()[-1])
-            status = line.split()[-2]
-            if int(status) in status_codes.keys():
-                status_codes[int(status)] += 1
-            line_count += 1
+def update_metrics(line, total_file_size, status_codes_stats):
+    '''Updates metrics from a given HTTP request log.
 
-            if line_count > 9:
-                do_print(total_size, status_codes)
-                line_count = 0
-    do_print(total_size, status_codes)
-except KeyboardInterrupt:
-    do_print(total_size, status_codes)
+    Args:
+        line (str): The line of input from which to retrieve the metrics.
+
+    Returns:
+        int: The new total file size.
+    '''
+    line_info = extract_input(line)
+    status_code = line_info.get('status_code', '0')
+    if status_code in status_codes_stats.keys():
+        status_codes_stats[status_code] += 1
+    return total_file_size + line_info['file_size']
+
+
+def run():
+    '''Start the log parser.
+    '''
+    line_num = 0
+    total_file_size = 0
+    status_codes_stats = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0,
+    }
+    try:
+        while True:
+            line = input()
+            total_file_size = update_metrics(
+                line,
+                total_file_size,
+                status_codes_stats,
+            )
+            line_num += 1
+            if line_num % 10 == 0:
+                print_statistics(total_file_size, status_codes_stats)
+    except (KeyboardInterrupt, EOFError):
+        print_statistics(total_file_size, status_codes_stats)
+
+
+if __name__ == '__main__':
+    run()
